d23b9cb58b2199758dc49c06a23ae43b5a9b0315
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-05-04 21:36:32 +0900

    :label: type: 型定義位置変更

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import type { ESLint, Linter } from 'eslint';
 import { version } from '../package.json';
 import { storeStateSuffix } from './rules/store-state-suffix';
 
+type RuleDefinitions = (typeof plugin)['rules'];
+
+export type RuleOptions = {
+  [K in keyof RuleDefinitions]: RuleDefinitions[K]['defaultOptions'];
+};
+export type Rules = {
+  [K in keyof RuleOptions]: Linter.RuleEntry<RuleOptions[K]>;
+};
+
 const plugin = {
   name: 'xeiculy',
   version,
@@ -11,12 +20,3 @@ const plugin = {
 } satisfies ESLint.Plugin;
 
 export default plugin;
-
-type RuleDefinitions = (typeof plugin)['rules'];
-
-export type RuleOptions = {
-  [K in keyof RuleDefinitions]: RuleDefinitions[K]['defaultOptions'];
-};
-export type Rules = {
-  [K in keyof RuleOptions]: Linter.RuleEntry<RuleOptions[K]>;
-};
